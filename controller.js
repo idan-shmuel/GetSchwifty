22af3835eab0ba087f6048ed2bc7335ffd49fb1b
idan shmuel <idanooshi1@gmail.com>
2022-07-03 19:57:39 +0300

    Added function that check if odd board is slovable

--- controller.js
+++ controller.js
@@ -9,3 +9,29 @@ function generateBoard(rowSize) {
     }
     return board;
 }
+
+function getGreaterNumbersCountInArray(board, i, j) {
+    let countGreaterNumbers = 0;
+    let continueCheckAfterNumber = j+1;
+    for (let k = i; k < board.length; k++) {
+        for (let l = continueCheckAfterNumber; l < board[0].length; l++) {
+            if (board[i][j] > board[k][l] && board[k][l] !== 0) {
+                countGreaterNumbers++;
+            }
+        }
+        continueCheckAfterNumber = 0;
+    }
+    return countGreaterNumbers;
+}
+
+function isOddBoardSolvable(board) {
+    let countGreaterNumbers = 0;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] !== 0){
+                countGreaterNumbers += getGreaterNumbersCountInArray(board, i, j);
+            }
+        }
+    }
+    return countGreaterNumbers%2 === 0;
+}
