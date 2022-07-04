98aaa9ef37502689ee9c98cf745531261b6460a7
idan shmuel <idanooshi1@gmail.com>
2022-07-04 15:22:43 +0300

    Added the function 'switchPlace' that move the selected element place

--- view.js
+++ view.js
@@ -7,6 +7,8 @@ function drawBoard(board) {
 
         rowData.forEach(function(cellData) {
             let cell = document.createElement('td');
+            cell.setAttribute("id", cellData);
+            cell.onclick = switchPlace;
             cell.appendChild(document.createTextNode(cellData));
             row.appendChild(cell);
         });
@@ -16,4 +18,56 @@ function drawBoard(board) {
 
     boardTable.appendChild(boardTableBody);
     document.body.appendChild(boardTable);
+
+    function switchPlace(event) {
+        let positionToSwipe = getPressedButton(event, board);
+        let indexY = positionToSwipe[0];
+        let indexX = positionToSwipe[1];
+
+        let emptyCellIndex = getIndexOfEmptyCell(board, indexY, indexX);
+        if (emptyCellIndex !== []) {
+            swipeBetweenTwoElements(board, indexY, indexX, emptyCellIndex);
+        }
+    }
 }
+
+function getPressedButton(event, board) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] == event.target.innerHTML) {
+                return [i,j];
+            }
+        }
+    }
+}
+
+function getIndexOfEmptyCell(board, indexY, indexX) {
+    if (board[indexY][indexX+1] === 0) {
+        return [indexY, indexX+1];
+    }
+    if (board[indexY+1][indexX] === 0) {
+        return [indexY+1, indexX];
+    }
+    if (board[indexY][indexX-1] === 0) {
+        return [indexY, indexX-1];
+    }
+    if (board[indexY-1][indexX] === 0) {
+        return [indexY-1, indexX];
+    }
+    return [];
+}
+
+function swipeBetweenTwoElements(board, indexY, indexX, emptyCellIndex) {
+    let tmpID = board[emptyCellIndex[0]][emptyCellIndex[1]];
+    let elementToSwap = document.getElementById(board[indexY][indexX])
+    let elementZero = document.getElementById(board[emptyCellIndex[0]][emptyCellIndex[1]])
+    elementToSwap.setAttribute("id", "temp");
+    elementZero.innerHTML = board[indexY][indexX];
+    elementZero.setAttribute("id", elementZero.innerHTML);
+    elementToSwap.setAttribute("id", tmpID);
+    elementToSwap.innerHTML = '0';
+
+    board[emptyCellIndex[0]][emptyCellIndex[1]] = board[indexY][indexX];
+    board[indexY][indexX]= 0;
+}
+drawBoard(([[6,0,7],[3,2,8],[4,5,1]]));
