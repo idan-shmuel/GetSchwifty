f3404956a3c1ce22622a2551723f714c7762c7c6
idan shmuel <idanooshi1@gmail.com>
2022-07-04 19:08:25 +0300

    Added the functions that relates to the board generate to the class 'GenerateBoard'

--- controller.js
+++ controller.js
@@ -1,33 +1,4 @@
-function getGreaterNumbersCountInArray(board, i, j) {
-    let countGreaterNumbers = 0;
-    let continueCheckAfterNumber = j+1;
-    for (let k = i; k < board.length; k++) {
-        for (let l = continueCheckAfterNumber; l < board[0].length; l++) {
-            if (board[i][j] > board[k][l] && board[k][l] !== 0) {
-                countGreaterNumbers++;
-            }
-        }
-        continueCheckAfterNumber = 0;
-    }
 
-    return countGreaterNumbers;
-}
-
-export function isBoardSolvable(board, isEven=false) {
-    let countGreaterNumbers = 0;
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            if (board[i][j] !== 0){
-                countGreaterNumbers += getGreaterNumbersCountInArray(board, i, j);
-            }
-            else if (isEven) {
-                countGreaterNumbers += i+1;
-            }
-        }
-    }
-
-    return countGreaterNumbers%2 === 0;
-}
 
 export function isGameFinished(board) {
     let expectedArray = [...Array(board.length * board[0].length).keys()];
--- controller/GenerateBoard.js
+++ controller/GenerateBoard.js
@@ -15,5 +15,37 @@ export class GenerateBoard {
 
         return board;
     }
+
+    getGreaterNumbersCountInArray(board, i, j) {
+        let countGreaterNumbers = 0;
+        let continueCheckAfterNumber = j+1;
+        for (let k = i; k < board.length; k++) {
+            for (let l = continueCheckAfterNumber; l < board[0].length; l++) {
+                if (board[i][j] > board[k][l] && board[k][l] !== 0) {
+                    countGreaterNumbers++;
+                }
+            }
+            continueCheckAfterNumber = 0;
+        }
+
+        return countGreaterNumbers;
+    }
+
+    isBoardSolvable(board, isEven=false) {
+        let countGreaterNumbers = 0;
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+                if (board[i][j] !== 0){
+                    countGreaterNumbers += this.getGreaterNumbersCountInArray(board, i, j);
+                }
+                else if (isEven) {
+                    countGreaterNumbers += i+1;
+                }
+            }
+        }
+
+        return countGreaterNumbers%2 === 0;
+    }
+
 }
 
